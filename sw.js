0959ce1ac4881c3a632cca74a2445a31344516ae
warriorgames <yasirunawanjana113@gmail.com>
2026-08-27 13:26:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,41 @@
-const CACHE_NAME = "masters-quizzes-v1";
+const CACHE_NAME = "masters-quizzes-v2";
+
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json"
+  "/termtestapp/",
+  "/termtestapp/index.html",
+  "/termtestapp/style.css",
+  "/termtestapp/script.js",
+  "/termtestapp/manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
+    caches.keys().then((keyList) => {
+      return Promise.all(
         keyList.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
